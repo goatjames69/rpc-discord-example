e555503d1021b7c7ce80480ab593df07bfa68dc7
goat james <129680954+goatjames69@users.noreply.github.com>
2023-07-28 21:17:49 +0545

    Add files via upload

---
+++ rpc-example/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rpc_example
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();   
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            discordrpc.Initialize();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
+//by goatjames
---
+++ rpc-example/discordrpc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Using DiscordRPC is mandatory, install it from Nuget Packages
+using DiscordRPC;
+
+namespace rpc_example
+{
+    internal class discordrpc
+    {
+        public static DiscordRpcClient client;
+        public static void Initialize()
+        {
+            // Create Application in https://discord.com/developers/applications and copy Application ID
+            client = new DiscordRpcClient("your application id");
+            client.Initialize();
+
+            client.SetPresence(new RichPresence()
+            {
+                Details = "NAME",
+                Assets = new Assets()
+                {
+                    LargeImageKey = "image link",
+                    LargeImageText = "Example Discord RPC",
+                    SmallImageKey = "image link",
+                    SmallImageText = "N/A",
+                },
+                Buttons = new DiscordRPC.Button[]
+                {
+                    new DiscordRPC.Button() { Label = "Discord", Url = "https://discord.gg/goatjames/" }
+                },
+                Timestamps = Timestamps.Now
+            });
+        }
+    }
+
+}
+//by goat james
